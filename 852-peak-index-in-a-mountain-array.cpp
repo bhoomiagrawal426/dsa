35bfcbcbeac3e4ef22678daa19dd15c40f9fe7a2
bhoomiagrawal426 <bhoomiagrawal426@gmail.com>
2026-07-02 08:16:59 +0530

    Add LeetCode 852 - Peak Index in a Mountain Array

---
+++ 852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start = 1;
+        int end = arr.size() - 2;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (arr[mid] > arr[mid - 1]) {
+                start = mid + 1; 
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;  // Should never reach here for valid mountain array
+    }
+};
